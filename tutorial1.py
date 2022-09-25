3a5cbfc91104d29e308180e432f8b5169842a654
Vince Nguyen <moomoo02@Vinces-MacBook-Pro.local>
2022-09-25 14:18:05 -0700

    Saved image

--- tutorial1.py
+++ tutorial1.py
@@ -2,7 +2,9 @@ import cv2
 
 img = cv2.imread('assets/elgato.jpeg', 1)
 img = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
-img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+#img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+cv2.imwrite('new_img.jpg', img)
+
 #-1, grb
 # 0, grayscale
 # 1, unchanged
