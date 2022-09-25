644652c595c1c1418dfa2fa7150e47942a857c2f
Vince Nguyen <moomoo02@Vinces-MacBook-Pro.local>
2022-09-25 14:16:49 -0700

    Sideways elgato

--- tutorial1.py
+++ tutorial1.py
@@ -2,7 +2,7 @@ import cv2
 
 img = cv2.imread('assets/elgato.jpeg', 1)
 img = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
-
+img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
 #-1, grb
 # 0, grayscale
 # 1, unchanged
