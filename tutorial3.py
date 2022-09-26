39157c5218bb4bf606bdf7119e2ae489a9b3d97c
Vince Nguyen <moomoo02@Vinces-MacBook-Pro.local>
2022-09-26 16:42:01 -0700

    showCamera

---
+++ tutorial3.py
@@ -0,0 +1,15 @@
+from ast import While
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
