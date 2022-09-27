5d300b3f403dcddc72169bea9b6645bf586a3368
Vince Nguyen <vincehnguyen@ucsb.edu>
2022-09-27 15:50:44 -0700

    draw

--- tutorial6.py
+++ tutorial6.py
@@ -3,12 +3,16 @@ import numpy as np
 import cv2
 
 img = cv2.imread('assets/chessboard.png')
-img = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.resize(img, (0,0), fx=1, fy=1)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 #cv2.goodFeaturesToTrack(source image, number of corners, minimum quality, euclidian distance)
-corners = cv2.goodFeaturesToTrack(img, 100, 0.01, 10)
+corners = cv2.goodFeaturesToTrack(gray, 100, 0.01, 10)
+corners = np.int0(corners) #convert to integers
 
+for corner in corners:
+    x,y = corner.ravel() #[[1,2], [3,4]] -> [1,2,3,4]
+    cv2.circle(img, (x,y), 5, (60,255,200), -1)
 
 cv2.imshow('Frame', img)
 cv2.waitKey(0)
