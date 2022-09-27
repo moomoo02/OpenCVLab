c0f819966f5977b2d038c20ac055e4d6b51be46e
Vince Nguyen <vincehnguyen@ucsb.edu>
2022-09-27 15:43:36 -0700

    addGrayScale

--- tutorial6.py
+++ tutorial6.py
@@ -3,6 +3,7 @@ import cv2
 
 img = cv2.imread('assets/chessboard.png')
 img = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 cv2.imshow('Frame', img)
 cv2.waitKey(0)
