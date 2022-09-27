dfa8240a2a2b6ca960c4eec15367418c652708d3
Vince Nguyen <vincehnguyen@ucsb.edu>
2022-09-26 17:11:18 -0700

    addedGrayRectangle

--- tutorial4.py
+++ tutorial4.py
@@ -8,10 +8,13 @@ while True:
     ret, frame = cap.read()
     width = int(cap.get(3))
     height = int(cap.get(4))
-
+    
+    #Lines: cv2.line(image, start, end, color, thickness)
     img = cv2.line(frame, (0,0), (width, height), (255,0,0), 10)
     img = cv2.line(frame, (width,0), (0, height), (255,0,0), 10)
 
+
+    img = cv2.rectangle(frame, (100,100), (200,200), (128,128,128), 5)
     cv2.imshow('frame', frame)
 
     #Breaks out when q is pressed
